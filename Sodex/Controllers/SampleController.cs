873cb1c8aa137b2e46667bdc6e8f585920ae99c8
Edison Llorada <edisonllorada@gmail.com>
2018-05-07 15:21:50 +0800

    Second commit

---
+++ Sodex/Controllers/SampleController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sodex.Controllers
+{
+    [RoutePrefix("api/Sample")]
+    public class SampleController : ApiController
+    {
+        [HttpGet, Route("List")]
+        public List<Models.Sample1> nameList() {
+            List<Models.Sample1> list = new List<Models.Sample1>();
+
+            list.Add(new Models.Sample1() { firstName = "Edison", lastName = "Llorada" });
+            list.Add(new Models.Sample1() { firstName = "Edisons", lastName = "Lloradas" });
+
+
+            return list;
+        }
+
+        [HttpPost, Route("Add")]
+        public HttpResponseMessage Add(Models.Sample1 obj) {
+            Debug.WriteLine(obj.firstName);
+
+            return Request.CreateResponse(HttpStatusCode.OK);
+        }
+    }
+}
---
+++ Sodex/Models/Sample1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sodex.Models
+{
+    public class Sample1
+    {
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+    }
+}
